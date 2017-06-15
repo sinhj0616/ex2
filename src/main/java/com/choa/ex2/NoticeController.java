9133678166713b458fb9e5fe9026d86dcd964d35
ssk-HP\ssk <guswns726@naver.com>
2017-06-15 20:38:10 +0900

    add Notice

---
+++ src/main/java/com/choa/ex2/NoticeController.java
@@ -0,0 +1,94 @@
+package com.choa.ex2;
+
+
+
+import java.util.List;
+
+import javax.inject.Inject;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.choa.notice.NoticeDTO;
+import com.choa.notice.NoticeService;
+
+@Controller
+@RequestMapping(value="/notice/**")
+public class NoticeController {
+	
+	@Inject  //type
+	private NoticeService noticeService;
+	
+	@RequestMapping(value="NoticeList" , method=RequestMethod.GET)
+	public void NoticeList(Model model, @RequestParam(defaultValue="1")Integer curPage)throws Exception{
+		List<NoticeDTO> ar =noticeService.noticeList(curPage);
+		model.addAttribute("list", ar);
+	}
+	
+	@RequestMapping(value="NoticeView" , method=RequestMethod.GET)
+	public void NoticeView(Model model,Integer num)throws Exception{
+	/*	if(num==null){
+			
+		}*/
+		NoticeDTO noticeDTO=noticeService.noticeView(num);
+		model.addAttribute("dto", noticeDTO);
+		//if 성공 실패
+		
+	}
+	//writeForm
+	@RequestMapping(value="NoticeWrite" , method=RequestMethod.GET)
+	public void NoticeWrite(){}
+	
+	
+	//write
+	@RequestMapping(value="NoticeWrite" , method=RequestMethod.POST)
+	public String NoticeWrite(NoticeDTO noticeDTO , RedirectAttributes rd)throws Exception{
+		int result=noticeService.noticeWrite(noticeDTO);
+		String message ="fail";
+		if(result>0){
+			message ="success";
+		}
+		//if 성공 실패
+		rd.addFlashAttribute("message", message);
+		return "redirect:NoticeList?curPage=2";
+	}
+	//updateForm
+		@RequestMapping(value="NoticeUpdate", method=RequestMethod.GET)
+		public void noticeUpdate(Integer num, Model model) throws Exception{
+			NoticeDTO noticeDTO = noticeService.noticeView(num);
+			model.addAttribute("dto", noticeDTO);
+		}
+	
+	
+	//update
+	@RequestMapping(value="NoticeUpdate", method=RequestMethod.POST)
+	public String noticeUpdate(NoticeDTO noticeDTO, RedirectAttributes rd) throws Exception{
+		int result = noticeService.noticeUpdate(noticeDTO);
+		System.out.println("값:" + noticeDTO);
+		String message = "FAIL";
+		if(result>0){
+			message="SUCCESS";
+		}
+		rd.addFlashAttribute("message", message);
+		return "redirect:NoticeList?curPage=1";
+	}
+	
+	@RequestMapping(value="NoticeDelete" , method=RequestMethod.GET)
+	public String NoticeDelete(Integer num , RedirectAttributes rd)throws Exception{
+		int result =noticeService.noticeDelete(num);
+		String message = "failed";
+		//if 성공 실패
+		if(result>0){
+			message ="success";
+		}
+		
+		rd.addFlashAttribute("message", message);
+		return "redirect:NoticeList";
+	}
+
+}
---
+++ src/main/java/com/choa/notice/NoticeService.java
@@ -0,0 +1,60 @@
+package com.choa.notice;
+
+
+import java.util.List;
+
+import javax.inject.Inject;
+
+import org.springframework.stereotype.Service;
+
+import com.choa.util.PageMaker;
+
+
+@Service
+//@Service의 의미 : Notice noticeDao =new NoticeDAO(); 와 같은 것 
+public class NoticeService {
+	
+	@Inject
+	private NoticeDAO noticeDAO;
+	
+	
+	/*//생성자(Constructor) 방식
+	public NoticeService(NoticeDAO noticeDAO) throws Exception{
+		this.noticeDAO =noticeDAO;
+	}
+	
+	//setter 방식 
+	public void setNoticeDAO(NoticeDAO noticeDAO) throws Exception{
+		this.noticeDAO = noticeDAO;
+	}*/
+	
+	
+	
+	//view 
+	public NoticeDTO noticeView(int num)throws Exception{
+		return noticeDAO.noticeView(num);
+		
+	}
+	
+
+	//list
+	public List<NoticeDTO> noticeList(int curPage)throws Exception{
+		int result =noticeDAO.noticeCount();
+		PageMaker pageMaker =new PageMaker(curPage);
+		return noticeDAO.noticeList(pageMaker.getRowMaker());
+	}
+	
+	//write 
+	public int noticeWrite(NoticeDTO noticeDTO)throws Exception{
+		return noticeDAO.noticeWrite(noticeDTO);
+	}
+	
+	//update
+	public int noticeUpdate(NoticeDTO noticeDTO)throws Exception{
+		 return noticeDAO.noticeUpdate(noticeDTO);
+	}
+	//delete
+	public int noticeDelete(int num)throws Exception{
+		return noticeDAO.noticeDelete(num);
+	}
+}
